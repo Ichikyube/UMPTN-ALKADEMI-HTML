2f92cf766bc7bf8de6c97029f670dd5ec803a7cb
Ichikyube <ichikyube@gmail.com>
2022-10-26 12:12:54 +0700

    add grunt-imgmin

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,50 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-    // Project configuration.
-    grunt.initConfig({
-      pkg: grunt.file.readJSON('package.json'),
-      uglify: {
-        options: {
-          banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-        },
-        build: {
-          src: 'src/<%= pkg.name %>.js',
-          dest: 'build/<%= pkg.name %>.min.js'
-        }
+  const mozjpeg = require('imagemin-mozjpeg');
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/<%= pkg.name %>.js',
+        dest: 'build/<%= pkg.name %>.min.js'
       }
-    });
-  
-    // Load the plugin that provides the "uglify" task.
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-  
-    // Default task(s).
-    grunt.registerTask('default', ['uglify']);
-  
-  };
+    },
+    imagemin: {
+      static: {
+        options: {
+          optimizationLevel: 3,
+          svgoPlugins: [{
+            removeViewBox: false
+          }],
+          use: [mozjpeg()] // Example plugin usage
+        },
+        files: {
+          'dist/img.png': 'src/img.png',
+          'dist/img.jpg': 'src/img.jpg',
+          'dist/img.gif': 'src/img.gif'
+        }
+      },
+      dynamic: {
+        files: [{
+          expand: true,
+          cwd: 'src/',
+          src: ['**/*.{png,jpg,gif}'],
+          dest: 'dist/'
+        }]
+      }
+    }
+  });
+
+
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  // Default task(s).
+  grunt.registerTask('default', ['uglify','imagemin']);
+
+};
